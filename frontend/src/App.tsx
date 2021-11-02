5735bf3c6c09eed06ab6d34f2cfa68834d69a990
Lucas Amaral <lucas.a.santos96@hotmail.com>
2021-11-01 22:33:35 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
+import Datatable from "componentes/Datatable";
+import Footer from "componentes/Footer";
+import NavBar from "componentes/NavBar";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá Mundo</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Olá Mundo</h1>
+
+        <Datatable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
